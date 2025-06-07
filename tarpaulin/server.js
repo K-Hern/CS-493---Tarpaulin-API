3e6d9eebef1367e4eb9b877ea55622f67763039b
Kevin Hernandez <KFHEDU@gmail.com>
2025-06-06 17:36:45 -0700

    Change compose to extract all env vars from .env

--- tarpaulin/server.js
+++ tarpaulin/server.js
@@ -3,7 +3,7 @@ const express = require('express')
 const api = require('./api')
 const { connectToDb } = require('./lib/mongo')
 const app = express()
-const port = process.env.PORT || 8000
+const port = process.env.PORT || 3000
 
 app.use(express.json())
 
